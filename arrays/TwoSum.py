8393e2e166919af32fed047771819d0073bd9c42
RubiHali <rubihali@@gmail.com>
2025-02-26 19:23:27 +0530

    [Data_Structure_Problem] Adds Two Sum problem solution with dictionary approach

--- arrays/TwoSum.py
+++ arrays/TwoSum.py
@@ -5,9 +5,15 @@ class TwoSum:
     Using Dictionary / Hash Map for solving this array problem
     """
     def find_pair(self, nums: list, target: int) -> list:
+        sum_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in sum_map:
+                return [sum_map.get(diff), i]
+            sum_map[n] = i
         return [-1, -1]
 
 
 if __name__ == '__main__':
     two_sum = TwoSum()
-    print(two_sum.find_pair([3, 4, 5, 6], 7))
+    print(two_sum.find_pair([3, 4, 5, 6], 11))
